080eba5a4999ba135dc1687ce5e59076022d2bf5
Vera <ilichovav@gmail.com>
2025-07-20 16:44:49 +0200

    game_slovo

---
+++ Vera Ilichova/classwork/game_Slovo/img/index7.js
@@ -0,0 +1,6 @@
+const img = document.querySelector("#imgWrapper>img");
+//console.log(img);
+//console.log(img);
+const word =document.getElementById("word");
+const inputLetter=document.getElementById("input");
+const button=document.getElementById("btn");
