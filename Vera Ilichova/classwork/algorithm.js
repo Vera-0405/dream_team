cdafa14361419bde68f040fda5fc85dd55e67325
Vera <ilichovav@gmail.com>
2025-08-14 09:18:15 +0200

    final Slovo Game and Pizza corr

--- Vera Ilichova/classwork/algorithm.js
+++ Vera Ilichova/classwork/algorithm.js
@@ -1,161 +1,101 @@
-<<<<<<< HEAD
-let one = [1 , 4];
-let two = [ 2, 8];
 
-//[1, 2, 2, 3, 4, 5, 6, 8, 9, 11]
-//Данная функция принимает два отсортированных массива и возвращает один отсортированный
+// let one = [1 , 4];
+// let two = [ 2, 8];
 
-function merge (one, two) {
-    let result = [];
-    let indexOne = 0;
-    let indexTwo = 0;
-    while(indexOne < one.length && indexTwo < two.length) {
-        if(one[indexOne] < two[indexTwo]) {
-           result.push(one[indexOne]);
-           indexOne++;
-        } else {
-            result.push(two[indexTwo]);
-            indexTwo++;
+// //[1, 2, 2, 3, 4, 5, 6, 8, 9, 11]
+// //Данная функция принимает два отсортированных массива и возвращает один отсортированный
+
+// function merge (one, two) {
+//     let result = [];
+//     let indexOne = 0;
+//     let indexTwo = 0;
+//     while(indexOne < one.length && indexTwo < two.length) {
+//         if(one[indexOne] < two[indexTwo]) {
+//            result.push(one[indexOne]);
+//            indexOne++;
+//         } else {
+//             result.push(two[indexTwo]);
+//             indexTwo++;
+//         }
+//     }
+
+//     return result.concat(one.slice(indexOne)).concat(two.slice(indexTwo));
+// }
+
+// console.log(merge(one, two));
+// let arr = [4, 1, 0 , -5 , 55, 32, 27, 9 , 12];
+
+// function mergeSort(arr) { // 38 27 43 3 // // 38 27
+//     //base case
+//     if(arr.length === 1) {
+//         return arr;
+//     }
+
+//     let mid = Math.floor(arr.length / 2);
+
+//     let leftArray = arr.slice(0, mid); // 38 27 43 3. // 38 27. // 38 // 3 
+//     let rightArray = arr.slice(mid); // 9 82 10.      // 43 3.  // 27 // 43
+
+//     let leftResultArray = mergeSort(leftArray); // 38 27 43 3 // 38 27 // 38 // 27 38
+//     // leftResultArray = 38
+//     // 27 38
+//     // 3, 27, 38, 43
+
+//     let rightResultArray = mergeSort(rightArray); // 27  // 43 3 //  9 82 10.
+//     // rightResultArray = 27
+//     // 3 43
+//     // 9 10 82
+
+//     let mergeResult = merge(leftResultArray, rightResultArray); // 27 38 // 3 43
+//     // 27, 38 and 3 , 43  =. 3, 27, 38, 43
+//     // 3, 27, 38, 43 and  9 10 82 =. 3, 9, 10, 27, 38, 43, 82
+
+//     return mergeResult;
+
+// }
+
+// console.log(mergeSort(arr));
+
+
+function findSumIndex(arr) {
+    for(let i = 0; i < arr.length; i++) { // n
+    
+        // i - index current element
+    
+        let sumLeft = 0;
+        let sumRight = 0;
+    
+        for(let j = 0; j < i; j++) {   // n
+            sumLeft = sumLeft + arr[j];
         }
-    }
+       
+        for(let k = i + 1; k < arr.length; k++) {  // n
+            sumRight = sumRight + arr[k];
+        }
+    
+        if(sumLeft == sumRight) {  // 1 
+            return i;
+        }
 
-    return result.concat(one.slice(indexOne)).concat(two.slice(indexTwo));
+       n * ( n + n) = 2n * n = n ^2
+    }
 }
 
-console.log(merge(one, two));
-let arr = [4, 1, 0 , -5 , 55, 32, 27, 9 , 12];
-
-function mergeSort(arr) { // 38 27 43 3 // // 38 27
-    //base case
-    if(arr.length === 1) {
-        return arr;
+function getPrefixIndex() {
+  let arr = [1, 7, 3, 6, 5, 6];
+  let sumValue = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumValue += arr[i];
+  }
+  let sumLeft = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // index = 0, value === 1, sumLeft === 0, sumRight = sumValue - value
+    let sumRight = sumValue - arr[i] - sumLeft;
+    if (sumRight === sumLeft) {
+      console.log(i);
+      break;
     }
-
-    let mid = Math.floor(arr.length / 2);
-
-    let leftArray = arr.slice(0, mid); // 38 27 43 3. // 38 27. // 38 // 3 
-    let rightArray = arr.slice(mid); // 9 82 10.      // 43 3.  // 27 // 43
-
-    let leftResultArray = mergeSort(leftArray); // 38 27 43 3 // 38 27 // 38 // 27 38
-    // leftResultArray = 38
-    // 27 38
-    // 3, 27, 38, 43
-
-    let rightResultArray = mergeSort(rightArray); // 27  // 43 3 //  9 82 10.
-    // rightResultArray = 27
-    // 3 43
-    // 9 10 82
-
-    let mergeResult = merge(leftResultArray, rightResultArray); // 27 38 // 3 43
-    // 27, 38 and 3 , 43  =. 3, 27, 38, 43
-    // 3, 27, 38, 43 and  9 10 82 =. 3, 9, 10, 27, 38, 43, 82
-
-    return mergeResult;
-
+    sumLeft += arr[i];
+  }
 }
-
-console.log(mergeSort(arr));
-=======
-//Sliding Window :
-
-//arr = [2, 1, 5, 1, 3, 2];  k = 3; (length of subarray)
-
-//2 1 5 = 8   // get , sum 
-//1 3 2 = 6
-//1 5 1 = 7
-//5 1 3 = 9   Max Sum = 9;
-
-//2 1 5
-//  1 5 1
-//    5 1 3
-//      1 3 2
-      
-//let maxSum = 0;      
-//for( let i = 0; i < arr.length-k; i++) { // 0 1
-    
-  //  let sum = 0;
-   // for(let j = i; j < i + 3; j++ ) { // 0 < 3, 1 < 4
-    
-    //    sum = sum + arr[j]; // arr[0] + arr[1] + arr[2] // arr[1] + arr[2] + arr[3]
-    
-    //}
-    //if( maxSum < sum) {
-    //    maxSum = sum;
-    //}
-    
-//}
-
-//let arr =  [2, 1, 5, 1, 3, 2]; 
-//let k = 3;
-//console.log(`Max sum  subarray is ${calculateMaxSumSubarray(arr, k)}`);
-
-//function calculateMaxSumSubarray(arr, k) {
-//    let maxSum = 0;
-//    let startIndex = 0; // индекс элемента , который отнимаем слева от суммы
-//    let currentSum = 0;
-
- //   for(let i = 0; i < arr.length; i++) { 
- //       if( i > k - 1 ) {  
-  //          if(maxSum < currentSum) { 
-    //            maxSum = currentSum; 
-      //      }
-      //      currentSum = currentSum - arr[startIndex]; 
-      //      startIndex++; 
-      //  }
-      //  currentSum = currentSum + arr[i];
-   // }
-   // if(maxSum < currentSum) {
-    //    maxSum = currentSum;
-   // }
-   // return maxSum;
-//}
-//let arr =  [2, 1, 5, 1, 3, 2]; 
-//let k = 3;
-//console.log(`Max sum  subarray is ${calculateMaxSumSubarray(arr, k)}`);
-
-//function calculateMaxSumSubarray(arr, k) {
-  //  let maxSum = 0;
-  //  let startIndex = 0; // индекс элемента , который отнимаем слева от суммы
-  //  let currentSum = 0;
-
-    //for(let i = 0; i < arr.length; i++) { 
-     //   if( i > k - 1 ) {  
-       //     if(maxSum < currentSum) { 
-        //        maxSum = currentSum; 
-        //    }
-        //    currentSum = currentSum - arr[startIndex]; 
-        //    startIndex++; 
-        //}
-        //currentSum = currentSum + arr[i];
-    //}
-    //if(maxSum < currentSum) {
-    //    maxSum = currentSum;
-    //}
-    //return maxSum;
-//}
-
-//let arr =  [2, 1, 5, 1, 3, 2]; 
-//let k = 3;
-//console.log(`Max sum  subarray is ${calculateMaxSumSubarray(arr, k)}`);
-
-//function calculateMaxSumSubarray(arr, k) {
-  //  let maxSum = 0;
-   // let startIndex = 0; // индекс элемента , который отнимаем слева от суммы
-   // let currentSum = 0;
-
-  //  for(let i = 0; i < arr.length; i++) { 
-  //      if( i > k - 1 ) {  
-   //         if(maxSum < currentSum) { 
-    //            maxSum = currentSum; 
-      //      }
-      //      currentSum = currentSum - arr[startIndex]; 
-        //    startIndex++; 
-      //  }
-       // currentSum = currentSum + arr[i];
-    //}
-    //if(maxSum < currentSum) {
-     //   maxSum = currentSum;
-   // }
-   // return maxSum;
-//}
->>>>>>> b93cb2f312d867dcea10d638a4178c53fef16f92
+getPrefixIndex();
--- Vera Ilichova/classwork/pizza/index.js
+++ Vera Ilichova/classwork/pizza/index.js
@@ -1,26 +1,104 @@
-// let clickValue = 0; // начало новой игры
-// let letterWordArr = [];
-// let inputValue = "";
-// let imageNumber = 1;
- const img = document.querySelector("#imgPizza>img");
- const formZakaz = document.getElementById("zakaz");
-// console.log(formZakaz);
- const inputLetter = document.getElementById("myInput");
- const button = document.getElementById("btn");
-// const button2 = document.getElementById("btn2");
-// // обработчики
-// button2.addEventListener("click", () => {
+let clickValueBase = 0;
+let clickValueIng1 = 0;
+let clickValueIng2 = 0;
+let clickValueSous = 0; // начало заказа
+let inputValue = "";
+let inputElement = "";
+let imageNumber = 1;
+const zakazArr = [];
+const img = document.querySelector("#imgPizza>img");
+const myZakaz = document.getElementById("zakaz");
+console.log(`Формирование заказа`);
+const button = document.getElementById("btn");
+// обработчики
+console.dir(document);
+const b = myZakaz.children[0];
+const ing1 = b.nextElementSibling;
+const ing2 = ing1.nextElementSibling;
+const s = ing2.nextElementSibling;
+
+b.addEventListener("click", (event) => {
+  clickValueBase++;
+  inputValue = event.target.innerText;
+  inputElement = b.children[0].innerText;
+  if (clickValueBase === 1) {
+    zakazArr.push(inputValue);
+    console.log(inputElement, inputValue);
+  }
+});
+ing1.addEventListener("click", (event) => {
+  clickValueIng1++;
+  inputValue = event.target.innerText;
+  inputElement = ing1.children[0].innerText;
+  if (clickValueIng1 <= 2) {
+    zakazArr.push(inputValue);
+    console.log(inputElement, inputValue);
+  }
+});
+ing2.addEventListener("click", (event) => {
+  clickValueIng2++;
+  inputValue = event.target.innerText;
+  clickValue = 0;
+  inputElement = ing2.children[0].innerText;
+  if (clickValueIng2 <= 2) {
+    zakazArr.push(inputValue);
+    console.log(inputElement, inputValue);
+  }
+});
+s.addEventListener("click", (event) => {
+  clickValueSous++;
+  inputValue = event.target.innerText;
+  inputElement = s.children[0].innerText;
+  if (clickValueSous === 1) {
+    zakazArr.push(inputValue);
+    console.log(inputElement, inputValue);
+  }
+});
+
+
+
+console.log(zakazArr);
+
+
+//  if (document.querySelectorAll("ol .ingredient1")) {console.log(ing1.innerText);}
+// if (document.querySelectorAll("ol .ingredient2")) {console.log( ing2.innerText);}
+// if (inputValue=== "инг1") { const inputArr= [...s.children]; console.log(inputArr);
+
+
+
+// button.addEventListener("click", () => {
 //   clickValue++;
 //   console.log(clickValue);
 //   if (clickValue === 1) {
 //     imageNumber = 1;
 //     letterWordArr = [];
 //     img.src = `./${imageNumber}.png`;
-//     getWord(letterWordArr);
+//     formZakaz();
 //   } else {
 //     clickValue--;
 //   }
 // });
-// inputLetter.addEventListener("input", () => {
-//   if (clickValue === 1) {
-//     inputValue = inputLetter.value;
+
+class Pizza {
+  constructor(base, ingredient1, ingredient2, price) {
+    this.base = base;
+    this.ingredient1 = ingredient1;
+    this.ingredient2 = ingredient2;
+    this.price = price;
+  }
+  run() {
+    console.log(
+      `pizza ${this.base} , ${this.ingredient1}, ${this.ingredient2} is prepearing`
+    );
+  }
+}
+
+const myPizza = new Pizza(`brot`, `wurst`, `pilze`, 5000);
+myPizza.run();
+//const myPizza= {
+// brand: "Margarita",
+// price: 5000,
+// ingredient: "ing1"}
+// myPizza.brand="Chilli";
+// console.log( `i have a ${myPizza.brand}`);
+// delete myPizza.price;
